b707bddad9b01108a30b1827e2b5f5657088f54f
Alessandro Rodrigues <alessandjr0@gmail.com>
2024-12-11 23:34:58 -0300

    commitando arquivos

---
+++ src/app/shared/modelo/aluno.ts
@@ -0,0 +1,9 @@
+export class Aluno {
+
+    constructor(public matricula: string='',
+                public nome: string = '',
+                public idade: number = 0,
+                public aluno: String = 'Alessandro Rodrigues de Souza Júnior'
+                ) {
+    }
+}
